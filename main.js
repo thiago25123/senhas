cc1ccd54e08efe40e0b7f678d2df5a73842e69d0
thiago25123 <147066617+thiago25123@users.noreply.github.com>
2025-05-22 11:36:54 -0300

    holfhfh

--- main.js
+++ main.js
@@ -61,16 +61,17 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
 
-function classificaSenha() {
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia= tamanhoSenha * matchMedia.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11) {
+    if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    } else if (entropia> 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
 }
